e15a97ec98b72d0adc5e9677b87095b767a166ce
zulhameffendy14 <zulhameffendy14@gmail.com>
2023-04-12 11:43:43 +0700

    css

--- src/resources/views/layouts/partials/stylesheets.blade.php
+++ src/resources/views/layouts/partials/stylesheets.blade.php
@@ -1,10 +1,10 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-<link rel="stylesheet" href="{{ asset('/vendor/fontawesome-free/css/all.min.css') }}">
+<link rel="stylesheet" href="{{ asset('adminlte::vendor/fontawesome-free/css/all.min.css') }}">
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-<link rel="stylesheet" href="{{ asset('/vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('/vendor/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
-<link rel="stylesheet" href="{{ asset('/vendor/select2/css/select2.min.css') }}">
-<link rel="stylesheet" href="{{ asset('/dist/css/adminlte.min.css') }}">
-<link rel="stylesheet" href="{{ asset('/css/custom.css') }}">
+<link rel="stylesheet" href="{{ asset('adminlte::vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('adminlte::vendor/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('adminlte::vendor/select2/css/select2.min.css') }}">
+<link rel="stylesheet" href="{{ asset('adminlte::dist/css/adminlte.min.css') }}">
+<link rel="stylesheet" href="{{ asset('adminlte::css/custom.css') }}">
 
 @stack('stylesheets')
